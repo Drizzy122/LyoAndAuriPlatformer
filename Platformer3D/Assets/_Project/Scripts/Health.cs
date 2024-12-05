38d5118071300aa4388defdee4314dabe78ebdc0
Drizzy_122 <92307762+Drizzy122@users.noreply.github.com>
2024-12-05 21:04:48 +0000

    unity added

---
+++ Platformer3D/Assets/_Project/Scripts/Health.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // For resetting the scene
+
+namespace Platformer {
+    public class Health : MonoBehaviour {
+        [SerializeField] int maxHealth = 100;
+        [SerializeField] FloatEventChannel playerHealthChannel;
+
+        int currentHealth;
+
+        public bool IsDead => currentHealth <= 0;
+
+        void Awake() {
+            currentHealth = maxHealth;
+        }
+
+        void Start() {
+            PublishHealthPercentage();
+        }
+
+        public void TakeDamage(int damage) {
+            currentHealth -= damage;
+            PublishHealthPercentage();
+
+            if (IsDead) {
+                HandleDeath();
+            }
+        }
+
+        void HandleDeath() {
+            if (gameObject.CompareTag("Enemy")) {
+                // Destroy the enemy game object
+                Destroy(gameObject);
+            } else if (gameObject.CompareTag("Player")) {
+                // Reset the scene if it's the player
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        void PublishHealthPercentage() {
+            if (playerHealthChannel != null)
+                playerHealthChannel.Invoke(currentHealth / (float)maxHealth);
+        }
+    }
+}
